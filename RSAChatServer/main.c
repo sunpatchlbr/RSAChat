6fe89542edadac8a9cdcaa29bba60bacf088caa5
sunpatchlbr <89615852+sunpatchlbr@users.noreply.github.com>
2024-01-02 16:22:41 -0600

    laying out more pseudocode
    going to have client structs,
    2 clients join a server, then chat begins,
    server only knows public keys, and sends public keys to each other client,
    client will know its own private key and public key, and also the other's public key

--- RSAChatServer/main.c
+++ RSAChatServer/main.c
@@ -3,9 +3,39 @@
 #include <stdio.h>
 #include <string.h>
 
-int main() {
-	
-	printf("Welcome to RSAChatServer, What would you like to do?:\n\n");
+//argument is port to host over
 
-	return 0;
+struct Client {
+	char name[10]
+	int socket = -1;
+	int publicKey = 1;
+}
+
+int main(int argc, char *argv[]) {
+	
+	if (argc == 1)	{
+		//socket
+
+		//bind
+
+		//listen
+
+		//accept
+
+		//fork to allow concurrent clients
+		
+		//ask for name and public key to send to others
+
+		//give them a "waiting string" while waiting for 2 clients to join
+
+		//once 2 are connected, give each other their public keys
+		//read write loop for each client sending encrypted data back and forth
+
+		//close
+		return 0;
+	}
+	else {
+		printf("Error, port number expected as argument");
+		return -1;
+	}
 } 
