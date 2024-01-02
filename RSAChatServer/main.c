7ceff9bb04cab5b3826502deed307d1285e9a6d9
sunpatchlbr <89615852+sunpatchlbr@users.noreply.github.com>
2024-01-02 16:11:41 -0600

    cleaning up files and beginning outline of project

---
+++ RSAChatServer/main.c
@@ -0,0 +1,11 @@
+//An attempt at a chat server using RSA Encryption
+
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+	
+	printf("Welcome to RSAChatServer, What would you like to do?:\n\n");
+
+	return 0;
+} 
