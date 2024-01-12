33d28003af4220961e90f530c5ec90e1dd57c099
sunpatchlbr <89615852+sunpatchlbr@users.noreply.github.com>
2024-01-12 14:16:10 -0600

    uncommitted things from a few days ago, getting a seg error, need to try hardcoding addresses to see if my pointers are problematic

--- RSAChatClient/main.c
+++ RSAChatClient/main.c
@@ -35,16 +35,21 @@ int main(int argc, const char *argv[]) {
 	//use internet address structure from inet/in.h
 	serverAddy.sin_family = AF_INET;
 	serverAddy.sin_port = htons(atoi(argv[1])); //use port from argument
-	serverAddy.sin_addr.s_addr = inet_pton(AF_INET,argv[2]);// use address string from arg2
-	
+	inet_pton(AF_INET, argv[2], &serverAddy.sin_addr.s_addr);// use address string from arg2 in pton to make the ipv4 address
 
 	//begin setting up socket for use with a single client
 	if (argc == 3)	{
+		
+		printf("port from arg[1]: %s\n", *argv[1]); //print port from arg1
+		char outAddy[INET_ADDRSTRLEN]; // declare string of ipv4 length
+		inet_ntop(AF_INET, &serverAddy.sin_addr, outAddy, INET_ADDRSTRLEN); // use ntop to create a string in outAddy of the ipv4 address
+		printf("address from arg[2]: %s\n", *outAddy);
+
+
 		int sockfd; //calling socket()
 		if ( ( sockfd = socket(AF_INET,SOCK_STREAM,IPPROTO_TCP) ) > -1 ) {//create generic sockaddr structure from the sockaddr_in at the address we made
-			if ( connect(sockfd, (struct sockaddr*)&serverAddy, NULL)) {
-				printf("connected to");
-				printf("%d", serverAddy.sin_addr.s_addr);
+			if ( connect(sockfd, (struct sockaddr*)&serverAddy, INET_ADDRSTRLEN)) {
+				printf("connected to %s\n", *outAddy);
 				printf("sending nickname and public key");
 				//send nickname as first line
 				//send public key as second line
